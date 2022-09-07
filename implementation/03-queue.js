34b1e79cbf9392c484e1550ba3f7b6a036eee486
Mark Gallant <markgallant01@gmail.com>
2022-09-07 17:59:41 -0400

    implement enqueue()

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,12 +5,25 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
+        
+        // check for empty list
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
 
-        // Write your hypothesis on the time complexity of this method here
+        this.length++;
+        return this.length;
     }
 
     dequeue() {
