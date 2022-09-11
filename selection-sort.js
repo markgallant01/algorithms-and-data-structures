3203de4d4e8614608f7ed5f8f071f27ebb44731b
Mark Gallant <markgallant01@gmail.com>
2022-09-10 20:12:34 -0400

    implement selectionSort()

--- selection-sort.js
+++ selection-sort.js
@@ -3,20 +3,33 @@
 function selectionSort(arr) {
 
   // Copy the original array
+  let unsorted = arr.slice();
 
   // Create an array to store the sorted values
+  let sorted = [];
 
   // While the array is not empty...
-
+  while (unsorted.length > 0) {
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minIndex = 0;
+    for (let i = 0; i < unsorted.length; i++) {
+      if (unsorted[i] < unsorted[minIndex]) {
+        minIndex = i;
+      }
+    }
 
     // Save and remove the value at the min index
+    let temp = unsorted[minIndex];
+    unsorted.splice(minIndex, 1);
 
     // Add the min value to the end of the sorted array
+    sorted.push(temp);
+  }
 
+  return sorted;
 }
 
 
