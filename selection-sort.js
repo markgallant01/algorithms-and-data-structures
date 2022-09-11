0db30f2f442fea711e965d66546038d2f8d91ae6
Mark Gallant <markgallant01@gmail.com>
2022-09-10 20:28:54 -0400

    implement selectionSortInPlace()

--- selection-sort.js
+++ selection-sort.js
@@ -37,21 +37,36 @@ function selectionSort(arr) {
 function selectionSortInPlace(arr) {
 
   // Set a pointer at zero diving the array into sorted and unsorted halves
+  let divide = 0;
 
   // Repeat while the unsorted half is not empty:
+  while (divide < arr.length) {
 
     // Do not move this console.log
     console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minIndex = divide;
+    for (let i = divide; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+    }
 
     // Save the min value
+    let minVal = arr[minIndex];
 
     // Shift every unsorted value to the left of the min value to the right by 1
+    for (let i = minIndex - 1; i >= divide; i--) {
+      arr[i+1] = arr[i];
+    }
 
     // Put the min value at the divider
+    arr[divide] = minVal;
 
     // Increment the divider and repeat
+    divide++;
+  }
 
 }
 
