eb2eec159f91e177903fbc468c932e62b1a3e795
Mark Gallant <markgallant01@gmail.com>
2022-09-11 22:19:25 -0400

    implement oddEvenSort()

--- custom-sorts.js
+++ custom-sorts.js
@@ -9,7 +9,13 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  arr.sort((a, b) => {
+    if (a % 2 === 0 && b % 2 === 1) { return 1; }
+    if (a % 2 === 1 && b % 2 === 0) { return -1; }
+    return a - b;
+  });
+
+  return arr;
 }
 
 function validAnagrams(s, t) {
