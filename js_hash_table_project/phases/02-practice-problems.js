8ba7d483b1baac601d8114aa826b714c8bb69471
Mark Gallant <markgallant01@gmail.com>
2022-09-13 19:12:42 -0400

    prepare for merge

--- js_hash_table_project/phases/02-practice-problems.js
+++ js_hash_table_project/phases/02-practice-problems.js
@@ -95,7 +95,16 @@ function duplicate(arr) {
 
 
 function twoSum(nums, target) {
-  // Your code here
+  // check each value with all the values after it
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 
